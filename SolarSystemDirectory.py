b76ad86a608f5fa63b17216fef706101b971816c
DANIEL <daniel@Someones-MacBook-Air.local>
2016-09-01 10:05:38 +0800

    commit all thing in 9_1_2016

--- SolarSystemDirectory.py
+++ SolarSystemDirectory.py
@@ -1,2 +1,15 @@
-x=input()
-print(x)
+"""
+This program shows the size and distance of the planes in the solar system with refrence to the earth
+the user enters the planet's name
+"""
+
+#int array[8]
+
+name =input()
+
+if name != ('Venus' || 'Mars' || 'Jupiter' || 'Neptune' || 'Saturn' || 'Mercury'):
+    print("Please enter the correct name of planets")
+
+else if name == Venus:
+    print("hi")
+
---
+++ new.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+import sys
+
+print(sys.argv)
+
+a = int (sys.argv[1]) # first argument
+b = int (sys.argv[2]) # second argument
+
+print ('the average is  ')
+print ((a+b)/2)
