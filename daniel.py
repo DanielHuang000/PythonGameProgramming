66dac44f254cdb8484a10767e47e053a4cd338e4
DANIEL <daniel@Someones-MacBook-Air.local>
2016-09-20 23:45:28 +0800

    add daniel.py

---
+++ daniel.py
@@ -0,0 +1,35 @@
+# This program will show my (name, e-mail, grade, and favorite lyric of a song) when import in another script
+
+a = True
+
+def all():
+    print ('Name: Daniel')
+    print ('')
+    print ('E-mail: danielhuang@pacificamerican.org')
+    print ('')
+    print ('Grade: 11')
+    print ('')
+    print ('Lyrics of my favorite song: Cant imagine life without having you here by my side no matter the cost.'
+    'When you just cant hold on, and the weight is too strong, Ill be there to break the fall.')
+    'When you feel confused by the world and all its mysteries'
+    'I can only hope you remember all the simple things.'
+
+    'Whats a heartbeat, with a heartache?'
+    'What a hurricane, without the rain?'
+    'Whats the moon without the stars'
+    'That how I feels when were apart'
+
+    'Whats a heartbeat, with a heartache?'
+    'What a hurricane, without the rain?'
+    'Whats the moon without the stars'
+    'That how I feels when were apart'
+
+    'What if it was easy as turning back the hands at time.'
+    'I wouldnt change a thing.'
+    'And I just wanna let you know, Ill never let you go.'
+    'Youll always be a part of me.'
+    'When you feel confused by the world and all its mysteries'
+    'I can only hope you remember all the simple things.')
+
+if a == True:
+    all()
